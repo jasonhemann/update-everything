1e1c239ca70a8ca026b45c1fc5c209385f1cc452
Jason Hemann <jasonhemann@gmail.com>
2021-06-16 07:54:10 -0400

    updating tldr pages when I update everything

--- update-everything.sh
+++ update-everything.sh
@@ -53,6 +53,8 @@ make install
 
 python3 -m pip install --user -r ~/Documents/neubanner/requirements.txt
 
+tldr --update 
+
 # Commented because I install fonts now with homebrew via tap cask-font.
 
 # python3 -m pip install afdko
